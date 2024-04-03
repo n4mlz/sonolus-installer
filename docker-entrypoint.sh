58d861591939c35a18639367f6406681973c5768
Nameless-itf23 <ohutoncat@gmail.com>
2024-04-04 01:03:05 +0900

    initial commit

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,33 +1,22 @@
 #!/bin/bash
 
+sonolus="Sonolus_0.7.5_1.ipa"
+
 printf "\nPlug in your idevice and enter your passcode.\n"
 printf "=============================================\n"
 while ! idevicepair pair; do
   sleep 1s
 done
 
-# Get SideStore ipa
-printf "\nDownloading SideStore.ipa stable.\n"
-printf "=================================\n"
-curl --progress-bar -L -o SideStore.ipa $(curl -s https://api.github.com/repos/SideStore/SideStore/releases/latest | grep "browser_download_url.*SideStore.ipa*" | cut -d : -f 2,3 | tr -d \")
-
-# Get SideStore-Nightly ipa
-printf "\nDownloading SideStore.ipa nightly.\n"
-printf "=================================\n"
-curl --progress-bar -L -o SideStore-Nightly.ipa https://github.com/SideStore/SideStore/releases/download/nightly/SideStore.ipa
+# Get Sonolus ipa
+printf "\nDownloading ${sonolus} \n"
+printf "=============================================\n"
+curl -#O "https://download.sonolus.com/${sonolus}"
 
 # Get udid
 udid=$(idevice_id | awk '{print $1}')
-
-# Generate mobiledevicepairing file
-printf "\nGenerating mobiledevicepairing file for SideStore\n"
-printf "===============================================================\n"
-./jitterbugpair -c > /mnt/${udid}.mobiledevicepairing && echo "Check your home folder on your host/after your exit, and copy the ${udid}.mobiledevicepairing file to your iDevice."
-
-echo -e "\nTo install an IPA, run the following command and change \033[0;31mmyemail\033[0m and \033[0;31mmyapplepass\033[0m. The \033[0;32mUDID\033[0m is already correct:"
-printf "=============================================================================================================\n"
-echo -e "./AltServer -u \033[0;32m${udid}\033[0m -a \033[0;31mmyemail@mail.com\033[0m -p \033[0;31mmyapplepass\033[0m SideStore.ipa\n"
-echo -e "Once you're finished, type: \033[0;35mexit\033[0m."
-printf "=================================\n\n"
-
-/bin/bash
+printf "\nInstalling ${sonolus} \n"
+printf "=============================================\n"
+read -p "Enter your Apple ID: " appleId
+read -sp "Enter your password: " pass
+./AltServer -u $udid -a $appleId -p $pass $sonolus
